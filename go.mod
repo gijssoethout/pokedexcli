4a668260d04a92fab410dfebf41eede826e68a33
Gijs <gijssoethout@github.com>
2025-06-26 15:01:03 +0200

    boot.dev chapter 1 complete

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gijssoethout/pokedexcli
+
+go 1.24.3
---
+++ repl.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		cmdName := words[0]
+		cmd, exists := getCommands()[cmdName]
+
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	lower := strings.ToLower(text)
+	words := strings.Fields(lower)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
